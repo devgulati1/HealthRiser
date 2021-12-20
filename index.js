1cad9bbc5b9d3883d5e9e426bee31bc9b4466e4f
Dev Gulati <gulatidev84@gmail.com>
2021-12-20 20:29:23 +0530

    done

---
+++ index.js
@@ -0,0 +1,10 @@
+let email=document.querySelector("#email")
+let password=document.querySelector("#password")
+let button=document.querySelector("button")
+
+button.addEventListener("click",()=>{
+    if(){
+        
+    }
+})
+
